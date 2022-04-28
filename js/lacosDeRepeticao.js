8684182b732289c6fdfe10afc2de3a459a1738cc
Nathália Elen de Souza Fialho de Araújo <n.fialho.de.araujo@avanade.com>
2022-04-28 11:48:33 -0300

    laços de repetição for e estrutura de incrementação

---
+++ js/lacosDeRepeticao.js
@@ -0,0 +1,12 @@
+let a = 0
+console.log(a = 0 + 1);//a = 1
+console.log(a = a + 1); //a = 1 + 1, a = 2
+console.log(a += 1);//a = 2 + 1, a = 3
+console.log(++a);// a = 3 + 1, a = 4
+
+for (let i = 0; i <= 10; i++) {
+    for (let j = 0; j <= 10; j++ {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+}
+
