f373bf123e39d3ca2a447464972002806da11c06
Nathália Elen de Souza Fialho de Araújo <n.fialho.de.araujo@avanade.com>
2022-04-28 14:24:32 -0300

    exemplos de estruturas condicionais e diferença de dois iguais e três iguais

---
+++ js/condicionais.js
@@ -0,0 +1,23 @@
+/* let a = Math.floor(Math.random() *100)
+console.log(a);
+
+if (a > 50) {
+  console.log('O valor de a está na metade maior da sequência');  
+} else if (a === 50){
+    console.log('O valor de a é igual a 50');
+} else {
+    console.log('O valor de a está na metade menor da sequência');
+} */
+
+//!diferença entre dois iguais e três iguais
+
+let a = 50
+let b = 50
+let c = '50'
+
+console.log(`Está comparação é: ${a == b} ou seja ${a} = ${b}!`);//comparação de valores
+console.log(`Está comparação é: ${a == c} ou seja ${a} = ${c}!`);//comparação de valores
+
+
+console.log(`Está comparação é: ${a === b} ou seja ${a} = ${b}!`);//comparação de valores e tipos
+console.log(`Está comparação é: ${a === c} ou seja ${a} !== ${c}!`);//comparação de valores e tipos
