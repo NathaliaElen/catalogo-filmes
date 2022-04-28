a40991683a4034c1efc865a0c806b0969687dee5
Nathália Elen de Souza Fialho de Araújo <n.fialho.de.araujo@avanade.com>
2022-04-28 13:44:37 -0300

    laço de repetição do... while

--- js/lacosDeRepeticao.js
+++ js/lacosDeRepeticao.js
@@ -1,4 +1,4 @@
-let a = 0
+/* let a = 0
 console.log(a = 0 + 1);//a = 1
 console.log(a = a + 1); //a = 1 + 1, a = 2
 console.log(a += 1);//a = 2 + 1, a = 3
@@ -8,5 +8,15 @@ for (let i = 0; i <= 10; i++) {
     for (let j = 0; j <= 10; j++ {
         console.log(`${i} * ${j} = ${i * j}`);
     }
-}
+} */
 
+/* let a = 0 
+while (a < 3) {
+    console.log('Bom dia!');
+    a++
+} */
+
+let a = 0
+do {
+   console.log(a); 
+} while (a > 0) 
