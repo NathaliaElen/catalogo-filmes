b00adfc58ce6dbbf3d20b448032eca4a4732e016
Nathália Elen de Souza Fialho de Araújo <n.fialho.de.araujo@avanade.com>
2022-04-29 09:14:11 -0300

    Começando a programação em javascript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
-function cadastrar() {
-    console.log('O filme foi cadastrado com sucesso!')
-}
+listaDeFilmes = [
+    'https://img.elo7.com.br/product/zoom/2657A05/big-poster-harry-potter-e-o-prisioneiro-de-azkaban-lo2-90x60-geek.jpg',
+]
+
+listaDeTitulos = [
+    'Harry Potter e o prisioneiro de azkaban',
+]
